f1d3c22921c05718b7883bcd054b99e6e5399744
josephquested <josephquested@gmail.com>
2016-06-23 10:32:15 +1200

    add new font

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 var app = document.getElementById('app')
-console.log(app)
+var page = 'home'
+
+var changePage = function (newPage) {
+  emptyNode(app)
+  page = newPage
+
+  switch (page) {
+    case "home":
+      renderHome()
+      break
+    case "games":
+      renderGames()
+      break
+    case "games":
+      renderContact()
+      break
+    }
+  }
+}
+
+var emptyNode = function (node) {
+  while (node.firstChild) {
+    node.removeChild(myNode.firstChild);
+  }
+}
+
+var renderHome = function () {
+  console.log('home!')
+}
+
+var renderGames = function () {
+  console.log('games!')
+}
+
+var renderHome = function () {
+  console.log('contact!')
+}
